98609e3fbc1f3cbfcd797fac010595fb62882fbc
hidekipinesol <hidekiagarie13@gmail.com>
2026-09-10 22:17:34 -0500

    Creación de la clase del player

---
+++ Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1_S3_Ejercicios
+{
+    internal class Player
+    {
+        private float life;
+        private float damage;
+
+        public Player(float life, float damage)
+        {
+            this.life = life;
+            this.damage = damage;
+        }
+
+        public void TakeDamage(float amount)
+        {
+            this.life -= amount;
+        }
+
+        public float DealtDamage()
+        {
+            return this.damage;
+        }
+    }
+}
