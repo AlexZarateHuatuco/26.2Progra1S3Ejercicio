ba719bab20ada81f8d24d568eb3caf2d5dcfb8c2
Alex Zarate <azarate45@gmail.com>
2026-09-24 15:37:31 -0500

    Create Entity.cs

---
+++ Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Progra1_S3_Ejercicios
+{
+    internal class Entity
+    {
+        protected int vida;
+        protected int daño;
+
+        public Entity(int vida, int daño)
+        {
+            this.vida = Math.Max(0, Math.Min(vida, 100));
+            this.daño = Math.Max(0, Math.Min(daño, 100));
+        }
+
+        public virtual int ObtenerDaño()        
+        {
+            return daño;
+        }
+
+        public virtual void RecibirDaño(int daño)
+        {
+            // ...
+        }
+    }
+}
